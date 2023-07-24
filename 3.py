9bdf0d24fe723e94666aaf51a43318dc82717f7b
paulinnik <83454680+paulinnik@users.noreply.github.com>
2023-07-24 21:04:48 +0300

    deleted useless

--- 3.py
+++ 3.py
@@ -4,7 +4,7 @@ from itertools import *
 k = 0
 for comb in product('01235689', repeat=6):
     p = ''.join(comb)
-    if p[0] == '2' and int(p[0]) % 3 != int(p[1]) % 3 and int(p[1]) % 3 != int(p[2]) % 3 and int(p[2]) % 3 != int(p[3]) % 3 and int(p[3]) % 3 != int(p[4]) % 3 and int(p[4]) % 3 != int(p[5]) % 3:
+    if p[0] == '2' and int(p[0]) % 3 != int(p[1]) % 3 and int(p[1]) % 3 != int(p[2]) % 3 and int(p[2]) % 3 != int(p[3]) % 3 \
+            and int(p[3]) % 3 != int(p[4]) % 3 and int(p[4]) % 3 != int(p[5]) % 3:
         k += 1
-        print(p)
 print(k)
